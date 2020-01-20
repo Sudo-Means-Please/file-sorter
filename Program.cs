ddbe5e2a036c06729820c31ae338c0cb8fe0b756
Decorvet <elias.decorvet.external@atos.net>
2020-01-20 13:56:03 +0100

    Added Startup-Class

--- Program.cs
+++ Program.cs
@@ -5,15 +5,20 @@ namespace Sorter
 {
     class Program
     {
+        // Add new Class to current Class //
         Sorting Sorting = new Sorting();
-        static void Main(string[] args)
+        // Main function //
+        static void Main()
         {
+            // Print Message for "Root-Folder input //
             Console.WriteLine("╔═══════════════════════════════════╗");
             Console.WriteLine("║       Enter Folder to sort:       ║");
             Console.WriteLine("╚═══════════════════════════════════╝");
             Console.Write("   >");
+            // Get "Root-Folder"
             string rootPath = Console.ReadLine();
 
+            // Print sorting-options //
             Console.WriteLine();
             Console.WriteLine("╔═══════════════════════════════════╗");
             Console.WriteLine("║      Select Option to sort by     ║");
@@ -25,18 +30,22 @@ namespace Sorter
             Console.WriteLine("║      [3]ModifiedDate              ║");
             Console.WriteLine("╚═══════════════════════════════════╝");
 
-
+            // Switch on users input //
             switch (Console.ReadLine()) 
             {
+                // Sort by Filetype //
                 case "1":
                     Sorting.SortByFileType(rootPath);
                     break;
+                // Sort by Filetype //
                 case "2":
                     Sorting.SortByLastUsed(rootPath);
                     break;
+                // Sort by Filetype //
                 case "3":
                     Sorting.SortByLastEdited(rootPath);
                     break;
+                // Print Error-Message and close //
                 default:
                     Console.WriteLine("╔═══════════════════════════════════╗");
                     Console.WriteLine("║    Illegal operation closing...   ║");
@@ -45,6 +54,7 @@ namespace Sorter
                     Environment.Exit(2);
                     break;
             }
+            // Print Success-Message //
             Console.WriteLine("╔═══════════════════════════════════╗");
             Console.WriteLine("║            ! All Done !           ║");
             Console.WriteLine("╚═══════════════════════════════════╝");
